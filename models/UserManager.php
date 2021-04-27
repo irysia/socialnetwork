8af1a6abd4d7ca8588286fe363da8563de820e9e
irysia <lau.rmblt@gmail.com>
2021-04-27 14:10:41 +0200

    register form ok

--- models/UserManager.php
+++ models/UserManager.php
@@ -20,3 +20,28 @@ function GetUserIdFromUserAndPassword ($username, $password){
   $response = $PDO -> query("SELECT * FROM user WHERE nickname = '$username' and password = '$password'");
   return $response->fetch();
 }
+
+function IsNicknameFree($nickname)
+{
+  global $PDO;
+  $response = $PDO->prepare("SELECT * FROM user WHERE nickname = :nickname ");
+  $response->execute(
+    array(
+      "nickname" => $nickname
+    )
+  );
+  return $response->rowCount() == 0;
+}
+
+function CreateNewUser($nickname, $password)
+{
+  global $PDO;
+  $response = $PDO->prepare("INSERT INTO user (nickname, password) values (:nickname , :password )");
+  $response->execute(
+    array(
+      "nickname" => $nickname,
+      "password" => $password
+    )
+  );
+  return $PDO->lastInsertId();
+}
