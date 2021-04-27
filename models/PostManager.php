524e028eb2739a4218601ab705f76bb1fcdcfcb6
irysia <lau.rmblt@gmail.com>
2021-04-27 12:25:27 +0200

    secure sql

--- models/PostManager.php
+++ models/PostManager.php
@@ -28,11 +28,23 @@ function GetAllPostsFromUserId($userId)
 
 function SearchInPosts($search){
   global $PDO;
-  $response = $PDO->query("SELECT post.*, user.nickname FROM post LEFT JOIN user ON (post.user_id = user.id) WHERE post.content like '%$search%' ORDER BY post.created_at DESC");
+  $response = $PDO->prepare("SELECT post.*, user.nickname FROM post LEFT JOIN user ON (post.user_id = user.id) WHERE post.content like :search ORDER BY post.created_at DESC");
+  $searchWithPercent = "%$search%";
+  $response->execute(
+    array(
+      "search" => $searchWithPercent
+    )
+  );
   return $response->fetchAll();
 }
 
-function CreateNewPost ($userId,$content){
+function CreateNewPost ($userId,$msg){
   global $PDO;
-  $PDO ->exec("INSERT INTO post (content,user_id) values ('$content',$userId)"); 
+  $response = $PDO ->prepare("INSERT INTO post (content,user_id) values ( :msg, :userId)"); 
+  $response->execute(
+    array(
+      "msg" => $msg,
+      "userId" => $userId
+    )
+  );
 }
